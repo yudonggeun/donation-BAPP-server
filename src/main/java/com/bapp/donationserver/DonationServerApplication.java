40d5094ac7616e975f75ee142632387a04213e8c
yudonggeun <ydong98@gmail.com>
2022-01-14 15:42:05 +0900

    create project

---
+++ src/main/java/com/bapp/donationserver/DonationServerApplication.java
@@ -0,0 +1,13 @@
+package com.bapp.donationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonationServerApplication.class, args);
+	}
+
+}
