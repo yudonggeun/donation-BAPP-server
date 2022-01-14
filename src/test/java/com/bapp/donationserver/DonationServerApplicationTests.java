fe5123405c40ae104ca4a3e5c38297eee2fb4a0b
yudonggeun <ydong98@gmail.com>
2022-01-14 15:42:05 +0900

    create project

---
+++ src/test/java/com/bapp/donationserver/DonationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bapp.donationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
