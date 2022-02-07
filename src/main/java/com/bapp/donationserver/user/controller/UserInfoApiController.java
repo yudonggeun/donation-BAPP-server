831911e4cb783db111d18c51026347cf7d31e7e9
yudonggeun <ydong98@gmail.com>
2022-02-07 09:42:50 +0900

    url 주석 추가

--- src/main/java/com/bapp/donationserver/user/controller/UserInfoApiController.java
+++ src/main/java/com/bapp/donationserver/user/controller/UserInfoApiController.java
@@ -10,16 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 public class UserInfoApiController {
-    @GetMapping
+   /* @GetMapping
     public String checkDonationHistory(){
         return "ok";
-    }
+    }*/
 
+    /**
+     * 클라이언트 : 사용자 id
+     * 응답 정보 : 표지이미지, 켐페인 제목, 재단 이름, 마감일, 현재 모금 금액, 목표 금액
+     */
     @GetMapping("{userId}")
     public String myDonationList(@PathVariable Long userId){
         return "ok";
     }
 
+    /**
+     * 기부 내역(거래 내역) : 사용처, 출금(환전)액, 거래 내용, 출금 시간
+     * 기부 내역 배열 전송
+     */
     @GetMapping("{campaignId}")
     public String checkCampaignHistory(@PathVariable String campaignId){
         return "ok";
